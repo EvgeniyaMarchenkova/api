1334edb8383f383ae5a1ba428ba43a8156ac7093
Evgeniya <evgeniova@gmail.com>
2017-01-20 21:20:49 +0300

    list of property added

--- app/controllers/CheckWeatherController.js
+++ app/controllers/CheckWeatherController.js
@@ -1,15 +1,27 @@
 var app = angular.module('myApp');
 
 app.controller('checkWeatherController', function ($scope,  $http) {
-    $scope.getWeather = function(){
-        $scope.results =[]; var newResults;
+
+    $scope.results =[];
+    $scope.getWeather = function() {
+
         $http.get("http://api.openweathermap.org/data/2.5/weather?q="+$scope.search+"&APPID=170fb5209f8eb67ee44eb90ccc45b4eb").then(
             function (response) {
-                $scope.results.push($scope.search);
-                $scope.results.push(response.data.sys.country);
-                $scope.results.push(response.data.main.temp-273);
-                $scope.results.push(response.data.weather[0].description);
-                $scope.iconUrl="http://openweathermap.org/img/w/"+response.data.weather[0].icon+".png";
+                $scope.resultResponse = {
+                    name:$scope.search,
+                    code:response.data.sys.country,
+                    temp:response.data.main.temp,
+                    description:response.data.weather[0].description,
+                    iconUrl:"http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png"
+
+
+                }
+             //   $scope.iconUrl = "http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png";
+                $scope.results.push($scope.resultResponse);
+
+
+
+
             }
         )
     }
