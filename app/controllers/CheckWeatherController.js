5e3bfafe6db75d6c8d4d37db4cfbaefbd29dda36
Evgeniya <evgeniova@gmail.com>
2017-01-20 22:11:36 +0300

    limiting thr quanity added

--- app/controllers/CheckWeatherController.js
+++ app/controllers/CheckWeatherController.js
@@ -4,7 +4,6 @@ app.controller('checkWeatherController', function ($scope,  $http) {
 
     $scope.results =[];
     $scope.getWeather = function() {
-
         $http.get("http://api.openweathermap.org/data/2.5/weather?q="+$scope.search+"&APPID=170fb5209f8eb67ee44eb90ccc45b4eb").then(
             function (response) {
                 $scope.resultResponse = {
@@ -13,18 +12,13 @@ app.controller('checkWeatherController', function ($scope,  $http) {
                     temp:response.data.main.temp,
                     description:response.data.weather[0].description,
                     iconUrl:"http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png"
-
-
-                }
-             //   $scope.iconUrl = "http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png";
+                };
                 $scope.results.push($scope.resultResponse);
-
-
-
 
             }
         )
+        if ($scope.results.length >= $scope.itemNumber) {
+            $scope.results =[];
+        }
     }
-
-
 })
