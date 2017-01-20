aa6643e95d31027de6cad7476feb538a3a741d2a
Evgeniya <evgeniova@gmail.com>
2017-01-20 23:17:15 +0300

    limiting of quanity added

--- app/controllers/CheckWeatherController.js
+++ app/controllers/CheckWeatherController.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp');
 
 app.controller('checkWeatherController', function ($scope,  $http) {
-
+    $scope.itemNumber =3;
     $scope.results =[];
     $scope.getWeather = function() {
         $http.get("http://api.openweathermap.org/data/2.5/weather?q="+$scope.search+"&APPID=170fb5209f8eb67ee44eb90ccc45b4eb").then(
